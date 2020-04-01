e260f6695b16b0e88fcaf7908ff4906e838d8905
Alex Clark <aclark@aclark.net>
2020-04-01 19:43:41 -0400

    Update

--- conf.py
+++ conf.py
@@ -67,6 +67,7 @@ html_theme_options = {
     "font_family": "Helvetica",
     "donate_url": "https://www.patreon.com/join/aclark4life/checkout?rid=4802806",
     "extra_nav_links": {"Now": "/now/index.html", "RSS": "/index.rss",},
+    "canonical_url" = "https://blog.aclark.net/"
 }
 
 html_sidebars = {
@@ -78,4 +79,3 @@ html_add_permalinks = "←"
 
 html_favicon = "images/favicon.ico"
 
-canonical_url = "https://blog.aclark.net/"
