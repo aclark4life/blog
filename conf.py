22cced5c3dc22b3a553a6beac854e24d4e951fc1
Alex Clark <aclark@aclark.net>
2020-03-31 20:31:51 -0400

    Update

--- conf.py
+++ conf.py
@@ -22,10 +22,10 @@ copyright = ' 2020 Alex Clark'
 author = 'Alex Clark'
 
 # The short X.Y version
-version = '0.0.1'
+version = ''
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
