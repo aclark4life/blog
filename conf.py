cd9e8c8e84c866f5123834df34b6869121aaaad0
Alex Clark <aclark@aclark.net>
2020-04-02 19:26:53 -0400

    Update

--- conf.py
+++ conf.py
@@ -68,7 +68,6 @@ html_theme_options = {
     "donate_url": "https://www.patreon.com/join/aclark4life/checkout?rid=4802806",
     "extra_nav_links": {"Now": "/now/index.html", "RSS": "/index.rss",},
     "github_user": "aclark4life",
-    "github_repo": "blog-2020",
 }
 
 html_sidebars = {
