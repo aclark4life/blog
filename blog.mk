179b1026aac9ff32cb453e14fe4134253956bf2e
Alex Clark <aclark@aclark.net>
2022-01-17 08:52:15 -0500

    Update

--- blog.mk
+++ blog.mk
@@ -1,5 +1,3 @@
 eb-deploy:
-	rm -rvf _build/
-	$(make) sphinx-build
 	aws s3 cp --recursive _build/html/ s3://blog.aclark.net
 	aws cloudfront create-invalidation --distribution-id ER61U0W7M90OK --paths "/*"
