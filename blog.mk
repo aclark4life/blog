ba6dac5c08b77f5f0114c95b1313aadbb43864e2
Alex Clark <aclark@aclark.net>
2023-11-19 13:06:47 -0500

    Update

--- blog.mk
+++ blog.mk
@@ -1,6 +1,6 @@
 deploy:
 	aws s3 cp --recursive _build/html/ s3://blog.aclark.net
-	aws cloudfront create-invalidation --distribution-id ER61U0W7M90OK --paths "/*"
+	aws cloudfront create-invalidation --distribution-id ER61U0W7M90OK --paths "/*" | cat
 
 deploy-clean:
 	aws s3 rm --recursive s3://blog.aclark.net/
