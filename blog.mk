07bbc763b90f01095b53bf94206db259504774f2
Alex Clark <aclark@aclark.net>
2023-11-24 20:32:07 -0500

    Update

--- blog.mk
+++ blog.mk
@@ -1,12 +1,8 @@
 deploy:
-	aws s3 rm --recursive s3://blog.aclark.net/*
+	aws s3 rm --recursive s3://blog.aclark.net/
 	aws s3 cp --recursive _build/html/ s3://blog.aclark.net
 	aws cloudfront create-invalidation --distribution-id ER61U0W7M90OK --paths "/*" | cat
 
 build:
 	rm -rvf _build/html/
 	$(MAKE) sphinx-build
-
-clean:
-	rm -rvf _build/
-	aws s3 rm --recursive s3://blog.aclark.net/
